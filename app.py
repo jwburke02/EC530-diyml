c4a6dce11ac02bfbfed72e3a8c0cf28335c61a53
jwburke02 <jwburke@bu.edu>
2024-03-03 16:11:06 -0500

    Updated actions

--- app.py
+++ app.py
@@ -14,17 +14,17 @@ app = Flask(__name__)
 api = Api(app)
 
 # add configured service endpoints  
-api.add_resource(AuthenticationAPI, '/auth') # Auth
-api.add_resource(DataAnalysisAPI, '/data_analysis') # image/label data info
-api.add_resource(AnalyzeModelAPI, '/analyze_model') # model results + scores
-api.add_resource(UploadDataAPI, '/upload/data_point') # datapoint upload (images + labels)
-api.add_resource(UploadClassAPI, '/upload/class_info') # class upload (list of strings)
-api.add_resource(UploadProjectAPI, '/upload/project') # project upload (new project document)
-api.add_resource(InferenceAPI, '/inference') # API for detection or classification using a trained model
-api.add_resource(PublishAPI, '/publish') # API for publishing a project as a model
-api.add_resource(ReportsAPI, '/reporting') # API for reporting model stats
-api.add_resource(TrainAPI, '/train') # API for training 
+api.add_resource(AuthenticationAPI, '/auth')  # Auth
+api.add_resource(DataAnalysisAPI, '/data_analysis')  # image/label data info
+api.add_resource(AnalyzeModelAPI, '/analyze_model')  # model results + scores
+api.add_resource(UploadDataAPI, '/upload/data_point')  # datapoint upload (images + labels)
+api.add_resource(UploadClassAPI, '/upload/class_info')  # class upload (list of strings)
+api.add_resource(UploadProjectAPI, '/upload/project')  # project upload (new project document)
+api.add_resource(InferenceAPI, '/inference')  # API for detection or classification using a trained model
+api.add_resource(PublishAPI, '/publish')  # API for publishing a project as a model
+api.add_resource(ReportsAPI, '/reporting')  # API for reporting model stats
+api.add_resource(TrainAPI, '/train')  # API for training
 
 # run on all available hosts locally on 7001
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=7001)
+    app.run(host='0.0.0.0', port=7001)
