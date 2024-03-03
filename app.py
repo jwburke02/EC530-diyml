7d15d15e48637e0485f9e3bbf02bbb62d79bf4f1
jwburke02 <jwburke@bu.edu>
2024-03-03 15:00:35 -0500

    Updated AUTH service - removed db work + stubbed out errors

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from services.auth import AuthenticationAPI
-from core import app, api
+from flask import Flask
+from flask_restful import Api
+
+app = Flask(__name__)
+api = Api(app)
 
 # add configured service endpoints  
 api.add_resource(AuthenticationAPI, '/auth')
--- services/auth.py
+++ services/auth.py
@@ -1,87 +1,43 @@
-from flask_restful import Resource, fields, marshal_with, reqparse
-import hashlib
-from random import randint
-
-from core import db
-
-class user(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(20), nullable = False)
-    hashed_pass = db.Column(db.String(200), nullable = False)
-    api_token = db.Column(db.String(200), nullable = False)
-
-    def __init__(self, id, username, hashed_pass, api_token):
-        self.id = id
-        self.username = username
-        self.hashed_pass = hashed_pass
-        self.api_token = api_token
-
-    def __repr__(self):
-        return self.username
-
-# Define data schema for user output
-auth_return = {
-    'uid' : fields.Integer,
-    'username' : fields.String,
-    'api_token' : fields.String,
-    'error_string' : fields.String
-}
-
-class FormatAuthReturn(object):
-    def __init__(self, uid, username, api_token, error_string):
-        self.uid = uid
-        self.username = username
-        self.api_token = api_token
-        self.error_string = error_string
-
-parser = reqparse.RequestParser().add_argument('username', help="Username cannot be blank...").add_argument('password', help="Password cannot be blank...").add_argument('uid', help="Must provide a valid uid...").add_argument('api_token', help="Must provide a valid api_token...")
-
-def generateUID():
-    min = 1
-    max = 100000
-    rand = randint(min, max)
-    while user.query.filter_by(id=rand).limit(1).first() is not None:
-        rand = randint(min, max)
-    return rand
+from flask_restful import Resource, reqparse
 
 class AuthenticationAPI(Resource):
-    @marshal_with(auth_return)
     def get(self):
         try:
+            parser = reqparse.RequestParser().add_argument('username', help="Username cannot be blank...", required=True).add_argument('password', help="Password cannot be blank...", required=True)
             args = parser.parse_args()
-            hashed_pass = hashlib.md5(args['password'].encode('UTF-8')).hexdigest()
-            result = user.query.filter_by(username=args['username'], hashed_pass=hashed_pass).first()
+            result = {
+                "username": args['username'],
+                "api_token": 'fake_token'
+            }
             if (result):
-                return FormatAuthReturn(result.id, result.username, result.api_token, "Successful")
+                return result, 200
             else:
-                return FormatAuthReturn(-1, "Error", "Error", "No user found...")
-        except Exception as e:
-            return FormatAuthReturn(-1, "Error", "Error", e)
-    @marshal_with(auth_return)
+                return {'Error' : 'no such user found'}, 404
+        except:
+            return "There was some issue with your request", 400
     def put(self):
         try:
+            parser = reqparse.RequestParser().add_argument('username', help="Username cannot be blank...", required=True).add_argument('password', help="Password cannot be blank...", required=True)
             args = parser.parse_args()
-            if user.query.filter_by(username=args['username']).limit(1).first() is not None:
-                return FormatAuthReturn(-1, "Error", "Error", "Username taken.")
-            hashed_pass = hashlib.md5(args['password'].encode('UTF-8')).hexdigest()
-            api_token = hashlib.md5(hashed_pass.encode('UTF-8')).hexdigest()
-            print(hashlib.md5(args['password'].encode('UTF-8')).hexdigest())
-            new_id = generateUID()
-            new_user = user(new_id, args['username'], hashed_pass, api_token)
-            db.session.add(new_user)
-            db.session.commit()
-            return FormatAuthReturn(new_id, args['username'], api_token, "Successful")
+            result = {
+                "username": args['username'],
+                "api_token": 'new_api_token'
+            }
+            return result, 200
         except Exception as e:
-            return FormatAuthReturn(-1, "Error", "Error", e)
+            return "There was some issue with your request", 400
     def delete(self):
         try:
+            parser = reqparse.RequestParser().add_argument('username', help="Username cannot be blank...", required=True).add_argument('password', help="Password cannot be blank...", required=True).add_argument('api_token', help="API token cannot be blank...", required=True)
             args = parser.parse_args()
-            result = user.query.filter_by(id=args['uid'], username=args['username'], hashed_pass=hashlib.md5(args['password'].encode('UTF-8')).hexdigest(), api_token=args['api_token']).first()
+            result = {
+                "username": args['username'],
+                "password": args['password'],
+                "api_token": args['api_token']
+            }
             if (result):
-                db.session.delete(result)
-                db.session.commit()
                 return {"Status": "Successful deletion"}, 200
             else:
                 return {'Error' : 'no such user found'}, 404
-        except Exception as e:
-            return e
+        except:
+            return "There was some issue with your request", 400
