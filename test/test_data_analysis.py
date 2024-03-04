660d4f35b79e4ad172d02313e3354a6f820ec9e3
jwburke02 <jwburke@bu.edu>
2024-03-04 15:33:14 -0500

    Fixed workflow (again) and added new doc for OpenAPI for data_analysis endpoint  + fixed minor errors

--- test/test_data_analysis.py
+++ test/test_data_analysis.py
@@ -6,14 +6,14 @@ from app import app
 def client():
     return app.test_client()
 
-def test_data_analysis_get(client: FlaskClient):
+def test_data_analysis_post(client: FlaskClient):
     # Happy Path
-    resp = client.get('/data_analysis', json={'api_token': 'token', 'project_name': 'name'})
+    resp = client.post('/data_analysis', json={'api_token': 'token', 'project_name': 'name'})
 
     assert resp.status_code == 200
     assert resp.json.get('DataAnalysisResults')
     assert resp.json.get('project_name')
 
     # Sad Path
-    resp = client.get('/data_analysis', json={'username': 'username'})
+    resp = client.post('/data_analysis', json={'username': 'username'})
     assert resp.status_code == 400
