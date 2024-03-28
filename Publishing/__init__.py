c2e8ea25daf8148bf881da3655f34da374463c34
John Burke <john@Johns-MacBook-Pro.local>
2024-03-28 01:36:46 -0400

    Fixing strange linting issue with f strings

--- Publishing/__init__.py
+++ Publishing/__init__.py
@@ -7,7 +7,8 @@ class PublishAPI(Resource):
             parser = reqparse.RequestParser().add_argument('api_token', help="API Token cannot be blank...", required=True).add_argument('project_name', help="Project Name cannot be blank...", required=True).add_argument('is_published', help="is_published Token cannot be blank...", required=True)
             args = parser.parse_args()
             DatabaseAccess.projectPublishing(args['project_name'], args['api_token'], args['is_published'])
-            return {'Status' : f'Operation successful, is_published set to {args['is_published']}'}, 200
+            is_pub = args['is_published']
+            return {'Status' : f'Operation successful, is_published set to {is_pub}'}, 200
         except Exception as e:
             print(e)
             return "There was some issue with your request", 400
